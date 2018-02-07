3ebc709847d24643068951407344ed3f0e6c9654
Charles Goettert <charles.a.goettert@gmail.com>
2018-02-07 00:19:19 -0200

    Initial commit

---
+++ src/main/java/com/mycompany/mavenproject2/Main.java
@@ -0,0 +1,25 @@
+package com.mycompany.mavenproject2;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Charles
+ */
+public class Main {
+
+    private static final EntityManagerFactory EMF = Persistence.createEntityManagerFactory("teste");
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        EntityManager em = EMF.createEntityManager();
+        
+        em.close();
+        EMF.close();
+    }
+
+}
---
+++ src/main/java/com/mycompany/mavenproject2/Usuario.java
@@ -0,0 +1,117 @@
+package com.mycompany.mavenproject2;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Charles
+ */
+@Entity
+@Table(name = "usuario")
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "nome")
+    private String nome;
+    @Column(name = "cpf")
+    private String cpf;
+    @Column(name = "login")
+    private String login;
+    @Column(name = "senha")
+    private String senha;
+    @Column(name = "email", columnDefinition = "text")
+    private String email;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Usuario)) {
+            return false;
+        }
+        Usuario other = (Usuario) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.cgoettert.framework.Usuario[ id=" + id + " ]";
+    }
+
+}
